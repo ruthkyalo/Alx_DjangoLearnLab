2f6b3bc0bd8af25a550381c5346e2757a45a9fed
Ruth <ruthkyalo21@gmail.com>
2025-07-20 21:56:03 +0300

    Set up URL routing for relationship_app and included it in the main project

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Book, Author, Library
+from django.views.generic import DetailView
 
-# Create your views here.
+# Function-Based View to list all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {'books': books})
+
+# Class-Based View for library detail
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
