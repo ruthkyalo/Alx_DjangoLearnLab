4dcfb804010bf6ee884b201284fd9ffb2896e675
Ruth <ruthkyalo21@gmail.com>
2025-08-03 20:36:50 +0300

    Implemented custom user model with additional fields and admin integration

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
 ]
 AUTH_USER_MODEL = 'users.CustomUser'  # Custom user model config
 
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
