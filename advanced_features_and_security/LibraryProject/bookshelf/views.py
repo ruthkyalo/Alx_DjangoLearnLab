9ffd34a21dd5bf99f099254389e164ec35c17353
Ruth <ruthkyalo21@gmail.com>
2025-08-03 21:55:42 +0300

    Add permission decorators to book-related views

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,27 +1,10 @@
-from django.shortcuts import render
-
-# advanced_features_and_security/views.py
+# bookshelf/views.py
 
 from django.contrib.auth.decorators import permission_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from .models import Book
 
-@permission_required('advanced_features_and_security.can_view', raise_exception=True)
-def view_books(request):
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
     books = Book.objects.all()
     return render(request, 'books/list.html', {'books': books})
-
-@permission_required('advanced_features_and_security.can_create', raise_exception=True)
-def create_book(request):
-    # logic to create book
-    pass
-
-@permission_required('advanced_features_and_security.can_edit', raise_exception=True)
-def edit_book(request, pk):
-    # logic to edit book
-    pass
-
-@permission_required('advanced_features_and_security.can_delete', raise_exception=True)
-def delete_book(request, pk):
-    # logic to delete book
-    pass
