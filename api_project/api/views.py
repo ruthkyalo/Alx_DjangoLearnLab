0a69b6dfc1dd6394a5a1acea4465bc988b75152d
Ruth <ruthkyalo21@gmail.com>
2025-08-03 23:05:48 +0300

    Implemented BookViewSet with CRUD operations using DRF ViewSet and DefaultRouter

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
+# ListAPIView (if required by previous task)
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# Full CRUD ViewSet
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
