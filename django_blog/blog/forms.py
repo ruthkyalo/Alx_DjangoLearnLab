722bd34aaf720c4baebce417b233e63d00b701fc
Ruth <ruthkyalo21@gmail.com>
2025-08-24 23:31:40 +0300

    Implement search feature: add form in base template and configure search URL

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post
 from .models import Comment
-
+from taggit.forms import TagWidget 
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -17,7 +17,10 @@ class CustomUserCreationForm(UserCreationForm):
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content', 'tags']
+        fields = ['title', 'content', 'tags']  # include tags field
+        widgets = {
+            'tags': TagWidget(),  # ensures proper tag input in form
+        }
 
 class CommentForm(forms.ModelForm):
     class Meta:
