ab8703a9f644147cb9b40d52b9d73068629c078b
Ruth <ruthkyalo21@gmail.com>
2025-08-31 20:24:00 +0300

    Implement core social media features: posts, comments, feed, user follow, likes, and notifications

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -135,6 +135,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# 
+# for production
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
