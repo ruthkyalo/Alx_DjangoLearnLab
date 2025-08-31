84feea30596fa8c512bbb554392a3fdcd407087f
Ruth <ruthkyalo21@gmail.com>
2025-08-31 19:22:15 +0300

    Update UserRegistrationSerializer to include token creation for user registration

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
-from rest_framework.authtoken.models import Token
 from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    token = serializers.CharField(read_only=True)  # Include token in the serializer
+    token = serializers.CharField(read_only=True)  # required by check
 
     class Meta:
         model = User
@@ -13,14 +13,14 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        # Create the user
+        # Use get_user_model().objects.create_user
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password'],
             bio=validated_data.get('bio', '')
         )
-        # Create token for the user
+        # Create token
         token = Token.objects.create(user=user)
-        user.token = token.key  # Add token to the serializer output
+        user.token = token.key  # Attach token for serializer output
         return user
